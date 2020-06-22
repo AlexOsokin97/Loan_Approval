1bc30242fef12086e24263d16c3b968aba8de709
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-06-22 13:29:23 +0300

    new files

--- untitled0.py
+++ untitled0.py
@@ -8,10 +8,32 @@ Created on Sun Jun 21 16:26:16 2020
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('loan.csv')
+data = pd.read_csv('loan.csv', low_memory=False)
 
 df = data.head(10)
 
-nans = {'Amount':data.isna().sum() , 'Percentage':(data.isna().sum()/len(data))*100}
-nans = pd.DataFrame(nans)
+nans = {'Amount':data.isna().sum() , 'Percentage':(data.isna().sum()/len(data))*100} 
+
+df = data.copy()
+
+columns = df.columns
+
+u = {}
+for col in columns:
+    u[col] = df[col].value_counts(dropna=False)
+    
+u = pd.DataFrame(u)
+
+head = df.head()
+
+s = df['term'].unique()
+
+
+s.sizes
+
+
+
+
+
+
 
